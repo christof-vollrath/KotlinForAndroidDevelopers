d7e0a6f803c37a5d60c652b6fd1569dfb5789c20
Christof Vollrath <christof@taobits.net>
2016-12-02 20:51:26 +0100

    RecyclerView and ListAdapger (Chapter 5)

---
+++ app/src/main/kotlin/net/taobits/kotlinforandroiddevelopers/MainActivity.kt
@@ -0,0 +1,48 @@
+package net.taobits.kotlinforandroiddevelopers
+
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.RecyclerView.Adapter
+import android.support.v7.widget.RecyclerView.ViewHolder
+import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.TextView
+
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        forecastList.layoutManager = LinearLayoutManager(this)
+        forecastList.adapter = ForecastListAdapter(items)
+    }
+}
+
+class ForecastListAdapter(val items: List<String>) : RecyclerView.Adapter<ForecastListAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(TextView(parent.context))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.textView.text = items[position]
+    }
+
+    override fun getItemCount(): Int = items.size
+
+    class ViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
+}
+
+private val items = listOf(
+    "Mon 6/23 - Sunny - 31/17",
+    "Tue 6/24 - Foggy - 21/8",
+    "Wed 6/25 - Cloudy - 22/17",
+    "Thurs 6/26 - Rainy - 18/11",
+    "Fri 6/27 - Foggy - 21/10",
+    "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/17",
+    "Sun 6/29 - Sunny - 20/7"
+)
